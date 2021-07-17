8598f4b3b3b78314aec61ef051d7ddc3dc6ea152
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2021-07-17 00:52:26 -0400

    index

--- archives/resources/js/index/quadros.index.setup.js
+++ archives/resources/js/index/quadros.index.setup.js
@@ -89,7 +89,6 @@ function setupGitHubApi() {
 
 function setupPhantomJs() {
     const apiKey = [
-        'a-demo-key-with-low-quota-per-ip-address',
         'ak-6mfhv-yvhbm-0cs14-8fdqp-7b0vp',
         'ak-gzrtr-8akne-kz20p-ev265-tej7k',
         'ak-13j42-79z7r-4vgww-k62m1-g22j9',

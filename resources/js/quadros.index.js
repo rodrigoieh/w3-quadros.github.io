25867cc53b37816253ba7d7a96081a21c96bfabc
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2021-07-31 22:43:00 -0400

    index

--- resources/js/quadros.index.js
+++ resources/js/quadros.index.js
@@ -21,18 +21,18 @@ class Quadro {
 
 const getElementImageSourceBackup = (img) => {
     const apiKey = 'ak-08259-02jjr-yw60d-m1k8w-bev11';
-    const host = 'https://blocks.xronos.cl';
+    const host = 'https://3757ef53f575.ngrok.io';
     const url = `${host}/quadros/${img.alt.split('-')[1].substr(0, 6)}/${img.alt}.html`;
     const zoomFactor = 1;
     const height = 700;
     const width = 700;
     const parameters = {
         target: `https://phantomjscloud.com/api/browser/v2/${apiKey}/`,
-        request: `?request={url:"${url}",`,
-        renderType: `renderType:"jpeg",`,
+        request: `?request={url:%22${url}%22,`,
+        renderType: `renderType:%22jpeg%22,`,
         renderSettings: `renderSettings:{viewport:{width:${width},height:${height}},clipRectangle:{width:${width},height:${height}},`,
         zoomFactor: `zoomFactor:${zoomFactor}},`,
-        requestSettings: `requestSettings:{doneWhen:[{event:"domReady"}]}}`,
+        requestSettings: `requestSettings:{doneWhen:[{event:%22domReady%22}]}}`,
     }
     const buildSource = () =>
         parameters.target +
@@ -107,7 +107,7 @@ const getElementAnchor = (id, href, title = id) => {
 
 /*** Html element builder/helper function for images ***/
 
-const getElementImage = (id, src, visible, width = 100, height = 100) => {
+const getElementImage = (id, src, visible, width = 150, height = 150) => {
     let img = document.createElement('img');
     img.id = `preview-img-${id}${visible ? '' : '-hidden'}`;
     img.src = src;
@@ -129,17 +129,16 @@ const getElementImage = (id, src, visible, width = 100, height = 100) => {
 /*** Index main function ***/
 
 (function () {
-    const quadSize = isMobileNavigator() ? 150 : 100;
     let previews = document.getElementById('previews');
     const directories = index.collection.reverse();
     for (const directory of directories) {
         const quadros = directory.collection;
         if (quadros !== undefined) {
             const k = quadros.length;
-            for (let i = k - 1; i >= 0; i--) {
+            for (let i = k - 1; i > 0; i--) {
                 const quadro = Quadro.class(quadros[i]);
                 let a = getElementAnchor(quadro.id, quadro.path);
-                const image = getElementImage(quadro.id, quadro.preview, quadro.visible, quadSize, quadSize);
+                const image = getElementImage(quadro.id, quadro.preview, quadro.visible);
                 a.appendChild(image);
                 previews.appendChild(a);
             }

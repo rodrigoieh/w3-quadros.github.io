6831fb41633cc49eef99c6d9d23973adab2c2a68
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2021-08-09 18:36:23 -0400

    quad

--- resources/js/quadros.index.data.js
+++ resources/js/quadros.index.data.js
@@ -1369,6 +1369,26 @@ const index =  {
           extension: ".html",
           directory: "quadros/202108",
           visible: true
+        },
+        {
+          id: "quad-20210809-2141-00.001",
+          type: "file",
+          path: "quadros/202108/quad-20210809-2141-00.001.html",
+          preview: "https://quad.xronos.cl/quadros/202108/quad-20210809-2141-00.001.jpeg",
+          filename: "quad-20210809-2141-00.001.html",
+          extension: ".html",
+          directory: "quadros/202108",
+          visible: true
+        },
+        {
+          id: "quad-20210809-2141-00.002",
+          type: "file",
+          path: "quadros/202108/quad-20210809-2141-00.002.html",
+          preview: "https://quad.xronos.cl/quadros/202108/quad-20210809-2141-00.002.jpeg",
+          filename: "quad-20210809-2141-00.002.html",
+          extension: ".html",
+          directory: "quadros/202108",
+          visible: true
         }
       ]
     },

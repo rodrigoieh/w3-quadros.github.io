3a9b9fc2758bf9cea7f39d0106e589d2908bc0a0
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2022-03-02 09:30:33 -0300

    202203

--- resources/scripts/run.cmd
+++ resources/scripts/run.cmd
@@ -1,1 +1,10 @@
-ngrok http
+rem previews
+cd C:\Dev\rodrigoieh\w3-quadros.previews.github.io
+start http 8001
+
+rem gallery
+cd C:\Dev\rodrigoieh\w3-quadros.github.io
+start http 8000
+
+rem start http://localhost:8001/quadros
+rem start http://localhost:8000/quadros

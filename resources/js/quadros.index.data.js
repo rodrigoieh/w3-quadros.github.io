3f4e2db6cf229011a1ff7a4c8575821dc4322d8e
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2022-05-21 09:26:45 -0400

    202205

--- resources/js/quadros.index.data.js
+++ resources/js/quadros.index.data.js
@@ -1644,6 +1644,26 @@ const index =  {
           extension: ".html",
           directory: "quadros/202205",
           visible: true
+        },
+        {
+          id: "quad-20220520-2323-00.000",
+          type: "file",
+          path: "quadros/202205/quad-20220520-2323-00.000.html",
+          preview: "http://localhost:8001/quadros/202205/quad-20220520-2323-00.000.jpeg",
+          filename: "quad-20220520-2323-00.000.html",
+          extension: ".html",
+          directory: "quadros/202205",
+          visible: true
+        },
+        {
+          id: "quad-20220520-2332-00.000",
+          type: "file",
+          path: "quadros/202205/quad-20220520-2332-00.000.html",
+          preview: "http://localhost:8001/quadros/202205/quad-20220520-2332-00.000.jpeg",
+          filename: "quad-20220520-2332-00.000.html",
+          extension: ".html",
+          directory: "quadros/202205",
+          visible: true
         }
       ]
     },
--- resources/js/quadros.index.js
+++ resources/js/quadros.index.js
@@ -43,7 +43,7 @@ class Quadro {
 
 const getElementImageSourceBackup = (img) => {
     const apiKey = 'ak-08259-02jjr-yw60d-m1k8w-bev11';
-    const host = 'https://805b-200-120-116-5.ngrok.io';
+    const host = 'https://9f31-200-104-75-160.ngrok.io';
     const url = `${host}/quadros/${img.alt.split('-')[1].substr(0, 6)}/${img.alt}.html`;
     const zoomFactor = 1;
     const height = 700;

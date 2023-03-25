95696404e3d954133c1cd763815bba62758657f8
Rodrigo Erazo Hermosilla <rodrigoerazo@gmail.com>
2023-03-25 10:33:06 -0300

    lenovo

--- resources/scripts/windows/gen_index.cmd
+++ resources/scripts/windows/gen_index.cmd
@@ -1,2 +1,2 @@
 rem switch branches: localhost, preview, main..
-cd C:\Dev\rodrigoieh\w3-quadros.github.io && node resources/node/quadros.index.js quadros > resources/js/quadros.index.data.js
+E: && cd E:\code\rodrigoieh\w3-quadros.github.io && node resources/node/quadros.index.js quadros > resources/js/quadros.index.data.js

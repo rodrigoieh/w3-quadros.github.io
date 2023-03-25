823f81403e75a9aec31a03cb138c6d14c739bd3f
Rodrigo Erazo Hermosilla <rodrigoerazo@gmail.com>
2023-03-25 10:32:46 -0300

    ubuntu

---
+++ resources/scripts/linux/gen_index.sh
@@ -0,0 +1,2 @@
+rem switch branches: localhost, preview, main..
+cd /mnt/hgfs/_shared/code/w3-quadros.github.io && node resources/node/quadros.index.js quadros > resources/js/quadros.index.data.js
---
+++ resources/scripts/windows/gen_index.cmd
@@ -0,0 +1,2 @@
+rem switch branches: localhost, preview, main..
+E: && cd E:\code\rodrigoieh\w3-quadros.github.io && node resources/node/quadros.index.js quadros > resources/js/quadros.index.data.js
